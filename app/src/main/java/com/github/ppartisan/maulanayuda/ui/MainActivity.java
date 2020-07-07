815cbe26f1176aaac0fce3466d0fc49976322dff
Asus <yudamaulana613@gmail.com>
2020-07-07 16:28:35 +0700

    Perfect

---
+++ app/src/main/java/com/github/ppartisan/maulanayuda/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.github.ppartisan.maulanayuda.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.ppartisan.maulanayuda.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
